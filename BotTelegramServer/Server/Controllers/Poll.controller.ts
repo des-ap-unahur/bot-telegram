60672ac8d248212f729c1941b5a2d350231fe552
cristians775 <cristiansaldivia775@gmail.com>
2020-06-24 14:23:10 -0300

    añadiendo metodos a poll controller

--- BotTelegramServer/Server/Controllers/Poll.controller.ts
+++ BotTelegramServer/Server/Controllers/Poll.controller.ts
@@ -2,10 +2,7 @@ import Poll from '../Models/Poll.model';
 
 class PollController {
   post = async (req: any, res: any): Promise<Poll> => {
-    const poll = await Poll.create(
-      /* { username: req.body.username, email: req.body.email, password: req.body.password },
-      { fields: ['username', 'email', 'password'] }, */
-    );
+    const poll = await Poll.create();
     return poll;
   }
   get = async (req: any, res: any): Promise<void> => {
@@ -13,7 +10,7 @@ class PollController {
     res.send();
   }
 
-  put = async(req:any, res:any): Promise<void>=>{
+  update = async(req:any, res:any): Promise<void>=>{
     const poll = await Poll.findOne();
     poll.update({});
     res.send();
@@ -24,6 +21,18 @@ class PollController {
     poll.destroy();
     res.send();
   }
+
+  getResponses = async (req:any, res:any): Promise<void> =>{
+    const pollsResponses = await Poll.findAll();
+    res.send(pollsResponses);
+  }
+
+  postResponses = async (req:any, res:any): Promise<void> =>{
+    const pollResponses = await Poll.create();
+    res.send();
+  }
+
 }
+
 
 export default new PollController();
