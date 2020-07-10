4822200d8ea0604a7f22fdc38b88715773d29b22
cristians775 <cristiansaldivia775@gmail.com>
2020-07-10 19:48:32 -0300

    añadiendo metodos al repository

--- BotTelegramServer/Server/Repositories/Poll.repository.ts
+++ BotTelegramServer/Server/Repositories/Poll.repository.ts
@@ -2,10 +2,25 @@ import Poll from "../Models/Poll.model";
 import PollQuestion from "../Models/PollQuestion.model";
 class PollRepository {
 
-  getPolls = async (req: any): Promise<any> => {
+  getPolls = async (): Promise<any> => {
     const polls :Array<Poll> = await Poll.findAll();
     return polls;
   };
+
+  getPoll = async (id: number): Promise<any> =>{
+    const poll: Poll = await Poll.findOne({where: {poll_id: id}});
+    return poll;
+  }
+
+  getPollsQuestions = async (): Promise<any> =>{
+    const pollsQuestions :Array<Poll>= await Poll.findAll({include: [PollQuestion]});
+    return pollsQuestions;
+  }
+  
+  getPollQuestionsById = async (id: number): Promise<any> =>{
+    const pollsQuestions :Poll= await Poll.findByPk(id,{include: [PollQuestion]});
+    return pollsQuestions;
 }
 
+}
 export default new PollRepository();
