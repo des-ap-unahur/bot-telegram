8a3e7dd8bfb32a89b3cf40214b9fd077d19a2507
cristians775 <cristiansaldivia775@gmail.com>
2020-07-10 19:46:38 -0300

    añadiendo metodos al controlador

--- BotTelegramServer/Server/Controllers/Poll.controller.ts
+++ BotTelegramServer/Server/Controllers/Poll.controller.ts
@@ -2,14 +2,41 @@ import PollRepository from "../Repositories/Poll.repository";
 import Poll from "../Models/Poll.model";
 
 class PollController {
-  
+
   getPolls = async (req: any, res: any): Promise<void> => {
     try {
-      const polls = await PollRepository.getPolls(req);
+      const polls = await PollRepository.getPolls();
       res.send(polls);
     } catch (e) {
       console.log(e);
     }
   };
+  
+  getPollById = async (req: any, res: any): Promise<void>=>{
+    try{
+      const poll = await PollRepository.getPoll(req.params.id);
+      res.send(poll);
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  getPollsQuestions = async (req: any, res: any): Promise<void>=>{
+    try{
+      const poll = await PollRepository.getPollsQuestions();
+      res.send(poll);
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  getPollQuestionsById = async (req: any, res: any): Promise<void>=>{
+    try{
+      const pollQuestions = await PollRepository.getPollQuestionsById(req.params.id);
+      res.send(pollQuestions);
+    }catch(e){
+      console.log(e);
+    }
+  }
 }
 export default new PollController();
