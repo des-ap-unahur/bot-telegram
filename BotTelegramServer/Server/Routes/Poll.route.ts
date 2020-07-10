5f1cd533de77870ba08185c3e17949b2491a1703
cristians775 <cristiansaldivia775@gmail.com>
2020-07-10 19:48:05 -0300

    añadiendo rutas

--- BotTelegramServer/Server/Routes/Poll.route.ts
+++ BotTelegramServer/Server/Routes/Poll.route.ts
@@ -4,9 +4,8 @@ const PollRoute = Router();
 
 
 PollRoute.get('/polls', PollController.getPolls);
-
-
-
-
+PollRoute.get('/poll/:id', PollController.getPollById);
+PollRoute.get('/polls/questions', PollController.getPollsQuestions);
+PollRoute.get('/poll/:id/questions', PollController.getPollQuestionsById);
 
 export default PollRoute;
