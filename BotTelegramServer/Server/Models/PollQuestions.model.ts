e1d9e236111d4288aec31dfca273d1c96e5cae8b
cristians775 <cristiansaldivia775@gmail.com>
2020-07-10 23:26:20 -0300

    cambiando atributos

---
+++ BotTelegramServer/Server/Models/PollQuestions.model.ts
@@ -0,0 +1,37 @@
+import { Model, Column, Table, CreatedAt, UpdatedAt, AutoIncrement, PrimaryKey, ForeignKey } from "sequelize-typescript";
+import { DataTypes } from "sequelize";
+import PollQuestionInterface from "../Interfaces/PollQuestion.interface";
+import Poll from "./Poll.model";
+
+@Table({
+  tableName: "Poll_questions",
+  timestamps: true,
+})
+
+class PollQuestion extends Model<PollQuestion>
+  implements PollQuestionInterface {
+  @AutoIncrement
+  @PrimaryKey
+  @Column(DataTypes.NUMBER)
+  poll_question_id?: number;
+
+  @ForeignKey(() => Poll)
+  @Column(DataTypes.NUMBER)
+  poll_id!: number;
+
+  @Column(DataTypes.STRING)
+  question!: string;
+
+  @Column(DataTypes.STRING)
+  description!: string;
+
+  @CreatedAt
+  @Column(DataTypes.DATE)
+  createdAt: Date;
+
+  @UpdatedAt
+  @Column(DataTypes.DATE)
+  updatedAt: Date;
+}
+
+export default PollQuestion;
