58088e92cc15b9719bc2365c54c7a37392d50325
Agustin12000 <agustin120000@gmail.com>
2020-07-23 09:10:58 -0300

    #BOT-71: añadiendo comandos referente a la registracion y contacto

--- BotTelegramServer/Server/Commands/Bot.commands.ts
+++ BotTelegramServer/Server/Commands/Bot.commands.ts
@@ -3,15 +3,22 @@ import { TelegrafContext } from 'telegraf/typings/context';
 import { typeCommands } from './TypeCommands.commands'; 
 import BotCommandRepository from '../Repositories/BotCommand.repository';
 import BotCommand from '../Models/BotCommands.model';
+import { ContactType } from './ContactType.command';
 
 export const buildCommands = async (bot: Telegraf<TelegrafContext>) => {
   const botCommands:BotCommand[] = await BotCommandRepository.getCommandsTypes();
   const commandsWithoutContact = botCommands.filter(command => command.commandsTypes.type !== "Contact")
+  const buildContactCommand = () => {
+    const commandContact = botCommands.find(command => command.commandsTypes.type === "Contact")
+    return ContactType.generateCommand(commandContact) 
+  }
+
 
   const commands = commandsWithoutContact.map(command => 
     { 
       const type = typeCommands.find(typeCommand => typeCommand.type === command.commandsTypes.type)
       const hasExternalParameter = command.commandsTypes.type === "NestedCommandsList";
+
       if(type){
         return !hasExternalParameter ?
           type.generateCommand(command)
@@ -25,10 +32,14 @@ export const buildCommands = async (bot: Telegraf<TelegrafContext>) => {
       }
     } 
   )
+  const commandStart = commands.find(command => typeof command === 'object' && command.message === "Start")
+  
+  bot.start(typeof commandStart === 'object' && commandStart.response);
 
-  bot.start(ctx => {});
   commands.map(command => typeof command === 'object' && bot.command(command.command, command.response))
   commands.map(command => typeof command === 'object' && bot.hears(command.message, command.response))
+
+  bot.on("contact", buildContactCommand())
   bot.launch();
 }
 
