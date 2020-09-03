a521d86d3bbe644deeda8472cfa4be93d7094913
cristians775 <cristiansaldivia775@gmail.com>
2020-09-02 22:49:20 -0300

    Eliminando try catch de los controladores

--- BotTelegramServer/Server/Controllers/BotCommand.controller.ts
+++ BotTelegramServer/Server/Controllers/BotCommand.controller.ts
@@ -6,99 +6,46 @@ import { botController } from '../Bot/Controller/Bot.controller';
 
 class BotCommandController {
   getCommands = async (req: any, res: any): Promise<void> => {
-    try {
       const botCommands: BotCommand[] = await BotCommandRepository.getAll();
       res.send(botCommands);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
   getCommandById = async (req: any, res: any): Promise<void> => {
     const { id } = req.params;
-
-    try {
       const botCommands: BotCommand = await BotCommandRepository.get(id);
       res.send(botCommands);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
   getCommandsTypes = async (req: any, res: any): Promise<void> => {
-    try {
       const botCommandsTypes: BotCommand[] = await BotCommandRepository.getCommandsTypes();
       res.send(botCommandsTypes);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
   
   getCommandsTypesById = async (req: any, res: any): Promise<void> => {
     const { id } = req.params;
-
-    try {
       const botCommandsTypes: BotCommand = await BotCommandRepository.getCommandsTypesById(id);
       res.send(botCommandsTypes);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
+
   };
 
   postCommand = async (req: any, res: any): Promise<void> => {
     const { body } = req;
-    
-    try {
       const botCommands: BotCommand = await BotCommandRepository.post(body)
       await botController.refreshCommands();
       res.send(botCommands);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
   updateCommand = async (req: any, res: any): Promise<void> => {
     const { body } = req;
     const { id } = req.params;
-
-    try {
       const botCommands: BotCommand = await BotCommandRepository.update(id, body);
       res.send(botCommands);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
   deleteCommand = async (req: any, res: any): Promise<void> => {
     const { id } = req.params;
-
-    try {
       await BotCommandRepository.delete(id);
       res.sendStatus(HttpStatus.OK);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
 }
--- BotTelegramServer/Server/Controllers/UserTypes.controller.ts
+++ BotTelegramServer/Server/Controllers/UserTypes.controller.ts
@@ -5,72 +5,34 @@ import { HttpStatus } from '../Config/Server/HTTPStatus.config';
 
 class UserTypesController {
   getUserTypes = async (req: any, res: any): Promise<void> => {
-    try {
       const userTypes: UserTypes[] = await UserTypesRepository.getAll();
       res.send(userTypes);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
   getUserTypeById = async (req: any, res: any): Promise<void> => {
     const { id } = req.params;
-
-    try {
       const userType: UserTypes = await UserTypesRepository.get(id);
       res.send(userType);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
   postUserType = async (req: any, res: any): Promise<void> => {
     const { body } = req;
-
-    try {
       const userType: UserTypes = await UserTypesRepository.post(body);
       res.send(userType);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
+
   };
 
   updateUserType = async (req: any, res: any): Promise<void> => {
     const { body } = req;
     const { id } = req.params;
-
-    try {
       const userType: UserTypes = await UserTypesRepository.update(id, body);
       res.send(userType);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 
   deleteUserType = async (req: any, res: any): Promise<void> => {
     const { id } = req.params;
-
-    try {
       await UserTypesRepository.delete(id);
       res.sendStatus(HttpStatus.OK);
-    } catch (e) {
-      res.send({
-        errorCodes: e, 
-        codeStatus: HttpStatus.INTERNAL_SERVER_ERROR
-      });
-    }
   };
 }
 
