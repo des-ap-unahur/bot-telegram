f77fabb519373f77d82f2cc65ccdffe447a9b022
jonthanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-09-08 20:10:48 -0300

    #BOT-67:agregando typeQuestion al modelo

--- BotTelegramServer/Server/Interfaces/BotResponseFiles.interface.ts
+++ BotTelegramServer/Server/Interfaces/BotResponseFiles.interface.ts
@@ -1,4 +1,5 @@
 interface BotResponseFiles{
+  bot_respose_files_id?: number | null;
   bot_response_id?: number | null;
   filename?: string | null;
   description?: string | null;
--- BotTelegramServer/Server/Interfaces/PollQuestion.interface.ts
+++ BotTelegramServer/Server/Interfaces/PollQuestion.interface.ts
@@ -2,6 +2,7 @@ interface PollQuestionInterface{
   poll_question_id?: number | null;
   poll_id?: number | null;
   question?: string | null;
+  type_question?: string | null;
   description?: string | null;
 }
    
