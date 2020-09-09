089ac3029e54ebb91197c34a1b51b023761df896
Agustin12000 <agustin120000@gmail.com>
2020-09-09 01:25:25 -0300

    #BOT-89: simplificando request validator

--- BotTelegramServer/Server/Middlewares/RequestValidator.middleware.ts
+++ BotTelegramServer/Server/Middlewares/RequestValidator.middleware.ts
@@ -2,36 +2,39 @@ import { transformAndValidate } from "class-transformer-validator";
 import { Request, Response, NextFunction } from 'express';
 import { HttpStatus } from "../Config/Server/HTTPStatus.config";
 
-const validateRequest = <T>(c: T, whitelist = true, errorHandler?: (err: any, req: Request, res: Response, next: NextFunction) => void) => {
+const validateRequest = <T>(classValidator: T, whitelist = true) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const toValidate = req.body
     if (!toValidate) {
-      if (errorHandler) {
-        errorHandler({ type: 'no-body' }, req, res, next)
-      } else {
-        res.status(HttpStatus.BAD_REQUEST).json({
+      res.status(HttpStatus.BAD_REQUEST)
+        .json({
           error: true,
           message: 'Validation failed',
           originalError: { message: 'No request body found' }          
         })
-      }
     } else {
-      transformAndValidate(c as any, toValidate, { validator: { whitelist } })
-        .then(transformed => {
-          req.body = transformed
-          next()
-        })
-        .catch(err => {
-          if (errorHandler) {
-            errorHandler(err, req, res, next)
-          } else {
-            res.status(HttpStatus.BAD_REQUEST).json({
-              error: true,
-              message: 'Validation failed',
-              originalError: err
-            })
+      transformAndValidate(
+        classValidator as any,
+        toValidate, 
+        { validator: { whitelist } }
+      )
+        .then(
+          transformed => {
+            req.body = transformed
+            next()
           }
-        })
+        )
+        .catch(
+          err => 
+          {
+            res.status(HttpStatus.BAD_REQUEST)
+              .json({
+                error: true,
+                message: 'Validation failed',
+                originalError: err
+              })
+          }
+        )
     }
   }
 }
