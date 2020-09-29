cd200c67a6148f2cd23e25d7eca1e4ed8386b353
Agustin12000 <agustin120000@gmail.com>
2020-09-29 14:05:12 -0300

    #BOT-118: agregando validacion a la registracion

--- BotTelegramServer/Server/Bot/Utils/RegistrationUser.utils.ts
+++ BotTelegramServer/Server/Bot/Utils/RegistrationUser.utils.ts
@@ -10,7 +10,7 @@ import BotUserTypeInterface from "../Interfaces/BotUserType.interface";
 export const registrationUser = async (ctx: TelegrafContext) => {
   const phoneNumber: number = Number(ctx.update.message.contact.phone_number.substr(-10));
   const guaraniUser: GuaraniUsersInterface | null = await GuaraniUsersRepository.getByPhoneNumber(phoneNumber);
-  const userType: BotUserTypeInterface = botUserTypes.find(type => type.name === guaraniUser.profile);
+  const userType: BotUserTypeInterface | null = guaraniUser && botUserTypes.find(type => type.name === guaraniUser.profile);
   const genericUserType: BotUserTypeInterface = !userType && botUserTypes.pop();
   const telegram_user_id: number = ctx.update.message.contact.user_id;
   const hasRegistered: BotUsersInterface | null = await BotUserRepository.getByTelegramIdWithGuaraniUser(telegram_user_id);
