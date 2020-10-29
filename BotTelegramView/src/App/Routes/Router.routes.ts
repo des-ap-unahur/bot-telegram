359b106fbd53873dc08578c509142f18972b1067
jonthanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-10-29 05:31:33 -0300

    agrego ruta registredUser

--- BotTelegramView/src/App/Routes/Router.routes.ts
+++ BotTelegramView/src/App/Routes/Router.routes.ts
@@ -1,13 +1,15 @@
 import Dashboard from '../Components/Dashboard/Dashboard.component';
 import GetPolls from '../Components/Polls/GetPolls/GetPolls.container';
 import GetUserTypes from '../Components/UserTypes/GetUserTypes/GetUserTypes.container';
+import GetRegisteredUsers from '../Components/RegisteredUsers/GetRegisteredUser/GetRegisteredUsers.container';
 import Route from '../Interfaces/Routes.interface';
 
 
 const publicRoutes: any[] = [
   {path: '/dashboard', component:Dashboard, exact:true},
   {path: '/dashboard/polls', component: GetPolls},
-  {path: '/dashboard/user-types', component: GetUserTypes}
+  {path: '/dashboard/user-types', component: GetUserTypes},
+  {path: '/dashboard/bot-subs-users', component: GetRegisteredUsers},
 ];
 
 const privateRoutes:Route[] = [
