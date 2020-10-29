b2e116676c0dc5c957535615c058747f704fd3a3
jonthanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-10-29 05:32:30 -0300

    agrego action a registeredUser

--- BotTelegramView/src/App/Store/Actions/RegisteredUser.action.ts
+++ BotTelegramView/src/App/Store/Actions/RegisteredUser.action.ts
@@ -5,32 +5,12 @@ const { actionsTypes, actionCreators } = builderAsyncActions([
         name: 'getBot-subs-users',
         endpoint: '/bot-subs-users',
         method: 'GET'
-    },
-    {
-        name: 'deleteBotSubUser',
-        endpoint: '/bot-subs-users/:param_1',
-        method: 'DELETE'
-    },
-    {
-        name: 'getBot-subs-users',
-        endpoint: '/bot-subs-users/:param_1',
-        method: 'PUT'
     }
 ])
 
-const syncActions = {
-    clearRegisteredUserStates: 'clear-botSubUsers-states',
-}
 
-const syncActionCreators = {
-    clearRegisteredUserStates: () => {
-        return {
-            type: syncActions.clearRegisteredUserStates
-        }
-    }
-}
 
 export default {
-    actionsTypes: Object.assign(actionsTypes, syncActions),
-    actionCreators: Object.assign(actionCreators, syncActionCreators)
-}
+    actionsTypes: Object.assign(actionsTypes),
+    actionCreators: Object.assign(actionCreators)
+  }
