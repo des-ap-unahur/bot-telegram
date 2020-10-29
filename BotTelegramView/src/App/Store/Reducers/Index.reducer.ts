7c6eace15bd18c873c36568194d48000193315eb
jonthanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-10-29 05:33:08 -0300

    agrego reducer de registeredUser

--- BotTelegramView/src/App/Store/Reducers/Index.reducer.ts
+++ BotTelegramView/src/App/Store/Reducers/Index.reducer.ts
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import auth from './Auth.reducer';
 import poll from './Poll.reducer';
 import userTypes from './UserTypes.reducer';
+import registeredUserReducer from './RegisteredUser.reducer';
 
 const appReducer = combineReducers(
   {
     auth,
     poll,
-    userTypes
+    userTypes,
+    registeredUserReducer,
   }
 )
 
@@ -18,4 +20,4 @@ export default (state:any, action:any) => {
     };
   }
   return appReducer(state, action)
-}
+} 
