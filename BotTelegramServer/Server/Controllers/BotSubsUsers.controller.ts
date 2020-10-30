6f84ba16c2bc55e8a2a3cac4abdc447a1bf69e14
jonthanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-10-30 01:35:46 -0300

    agrego paginacion

--- BotTelegramServer/Server/Controllers/BotSubsUsers.controller.ts
+++ BotTelegramServer/Server/Controllers/BotSubsUsers.controller.ts
@@ -6,8 +6,15 @@ import execDelete from '../Utils/ExecDelete.utils';
 
 class BotSubsUsersController {
   getBotSubsUsers = async (req: any, res: any): Promise<void> => {
-    const botSubsUsers: BotSubsUsers[] = await BotsubsUsersRepository.getAll();
+    const paginationData = req.query;
+    if(paginationData.page){
+    const botSubsUsers= await BotsubsUsersRepository.getAllWithPagination(paginationData);
     res.send(botSubsUsers);
+  } else{
+    const botSubsUsers:BotSubsUsers[]= await BotsubsUsersRepository.getAll();
+    res.send(botSubsUsers);
+  }
+    
   };
 
   getBotSubsUserById = async (req: any, res: any): Promise<void> => {
