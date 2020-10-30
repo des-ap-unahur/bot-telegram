f5a12c45621ef00499bd883943d3a760060b24d7
jonthanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-10-30 01:26:52 -0300

    arreglo ruta subscribers

--- BotTelegramView/src/App/Routes/Router.routes.ts
+++ BotTelegramView/src/App/Routes/Router.routes.ts
@@ -1,7 +1,7 @@
 import Dashboard from '../Components/Dashboard/Dashboard.component';
 import GetPolls from '../Components/Polls/GetPolls/GetPolls.container';
 import GetUserTypes from '../Components/UserTypes/GetUserTypes/GetUserTypes.container';
-import GetRegisteredUsers from '../Components/RegisteredUsers/GetRegisteredUser/GetRegisteredUsers.container';
+import GetBotSubsUsers from '../Components/BotSubsUsers/GetBotSubsUsers/GetBotSubsUsers.container';
 import Route from '../Interfaces/Routes.interface';
 
 
@@ -9,7 +9,7 @@ const publicRoutes: any[] = [
   {path: '/dashboard', component:Dashboard, exact:true},
   {path: '/dashboard/polls', component: GetPolls},
   {path: '/dashboard/user-types', component: GetUserTypes},
-  {path: '/dashboard/bot-subs-users', component: GetRegisteredUsers},
+  {path: '/dashboard/subscribers', component: GetBotSubsUsers},
 ];
 
 const privateRoutes:Route[] = [
