c349481f7f1d45791fe2747d4882c2ac7c21ad99
jonthanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-11-06 00:35:53 -0300

    traigo botUsers con include

--- BotTelegramServer/Server/Entities/Repositories/BotSubsUsers.repository.ts
+++ BotTelegramServer/Server/Entities/Repositories/BotSubsUsers.repository.ts
@@ -2,11 +2,15 @@ import BotSubsUsers from "../Models/BotSubsUsers.model";
 import BotSubsUsersInterface from "../../Interfaces/BotSubsUsers.interface";
 import paginate from "../../Utils/Paginate.utils";
 import BotSubsUsersWithPagination from "../../Interfaces/BotSubsUsersWithPagination.interface"
+import BotUsers from "../Models/BotUsers.model";
+import UserTypes from "../Models/UserTypes.model";
+ 
 
  
 class BotSubsUsersRepository {
   getAllWithPagination = async (paginationData: any): Promise<BotSubsUsersWithPagination> => {
     const {count, rows: botSubsUsers} = await BotSubsUsers.findAndCountAll({
+      include:[{model: BotUsers, include:[UserTypes]}],
       ...paginate(paginationData)
     })
     const total = count;
